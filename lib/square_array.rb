acc5e9d0a223cbc5e092d8a6e674bb4eafb26333
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2020-12-31 19:33:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,7 @@ def square_array(array)
  count=0
  new_array=[]
   while array[count] do
-puts array[count]**2
-new_array.push(array[count])
+new_array.push(array[count]**2)
 count+=1 
 end
 new_array
